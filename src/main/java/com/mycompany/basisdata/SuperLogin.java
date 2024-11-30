7976be08efe1a44c95aac4ff436fb47fda87b762
Alfredrown <alfredoradhinalm@gmail.com>
2024-11-30 15:12:35 +0700

    fixed login email issue

--- src/main/java/com/mycompany/basisdata/SuperLogin.java
+++ src/main/java/com/mycompany/basisdata/SuperLogin.java
@@ -239,6 +239,9 @@ public class SuperLogin extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
+    public String getEmail() {
+    return EmailField.getText();  // Retrieve the text from the EmailField JTextField
+}
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
